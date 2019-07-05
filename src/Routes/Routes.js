98ab476cb678f18c02fdae93da08ac5c64366525
Johnisaig <isaigmichael30@gmail.com>
2019-07-06 01:46:24 +0800

    practice

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 
 import Home from '../Forms/Home.js';
 import About from '../Forms/About.js';
@@ -8,12 +8,12 @@ class Routes extends Component {
 
     render(){
         return(
-          <BrowserRouter>
+          <HashRouter>
               <React.Fragment>
                 <Route path="/home" component={Home} exact/>
                 <Route path="/about" component={About} exact/>
             </React.Fragment>
-          </BrowserRouter>
+          </HashRouter>
         );
     }
 }
